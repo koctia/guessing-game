ba394837c8ba98c5b26c3b193b349924f65658fa
Konstantin Moskvin <mr.koctia@gmail.com>
2022-07-15 14:48:46 +0300

    the GuessingGame task has been completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,25 +1,25 @@
-class GuessingGame { // алгоритм https://en.wikipedia.org/wiki/Binary_search_algorithm
+class GuessingGame { // algorithm https://en.wikipedia.org/wiki/Binary_search_algorithm
   constructor() {
     this.minNumber = 0;
     this.maxNumber = 0;
     this.middleNumber = 0;
   }
 
-  setRange(min, max) { // этот метод принимает min и max значение диапазона чисел для угадывания
+  setRange(min, max) { // this method accepts min and max value of range of number to guess
     this.minNumber = min;
     this.maxNumber = max;
   }
 
-  guess() { // этот метод возвращает кандидата на решение (вы делаете предположение, основанное на диапазоне и предыдущих предположениях)
+  guess() { // this method returns solution candidate (you make an assumption based on range and previous assumptions)
     this.middleNumber = Math.ceil((this.minNumber + this.maxNumber) / 2);
     return this.middleNumber;
   }
 
-  lower() { // этот метод вызывается, если предыдущий вызов guess() вернул число, которое больше, чем ответ
+  lower() { // this method is called if prev call of guess() returned number which is greater than answer
     this.maxNumber = this.middleNumber;
   }
 
-  greater() { // этот метод вызывается, если предыдущий вызов guess() вернул число, которое меньше, чем ответ
+  greater() { // this method is called if prev call of guess() returned number which is lower than answer
     this.minNumber = this.middleNumber;
   }
 }
